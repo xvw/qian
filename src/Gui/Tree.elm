277c2d890c0b11a3976f92e639f7326f1c818629
xvw <xavier.vdw@gmail.com>
2017-11-11 17:32:31 +0100

    Append Parent-button

--- src/Gui/Tree.elm
+++ src/Gui/Tree.elm
@@ -8,17 +8,17 @@ import Html.Events exposing (onClick)
 import Gui.Helper exposing (icon)
 
 
-entryDirectory : Path -> File -> ( Html Message, List (Attribute Message) )
-entryDirectory path file =
+entryDirectory : Path -> ( Html Message, List (Attribute Message) )
+entryDirectory path =
     let
         message =
-            ChangeDirectory (path ++ [ file.name ])
+            ChangeDirectory path
     in
         ( icon "folder", [ Attr.class "tree-elt folder", onClick message ] )
 
 
-entryFile : Path -> File -> ( Html Message, List (Attribute Message) )
-entryFile path file =
+entryFile : Path -> ( Html Message, List (Attribute Message) )
+entryFile path =
     ( icon "file", [ Attr.class "tree-elt not-folder" ] )
 
 
@@ -27,11 +27,34 @@ entry path file =
     let
         ( fileIcon, attributes ) =
             if file.isDirectory then
-                entryDirectory path file
+                entryDirectory (path ++ [ file.name ])
             else
-                entryFile path file
+                entryFile (path ++ [ file.name ])
     in
         a attributes [ fileIcon, text file.name ]
+
+
+parentFolder : Model -> List (Html Message)
+parentFolder model =
+    case List.reverse model.history.present of
+        [] ->
+            []
+
+        [ _ ] ->
+            []
+
+        _ :: xs ->
+            let
+                path =
+                    List.reverse xs
+
+                message =
+                    ChangeDirectory path
+            in
+                [ a
+                    [ Attr.class "tree-elt folder parent", onClick message ]
+                    [ icon "level-up", text (String.join "/" path) ]
+                ]
 
 
 render : Model -> Html Message
@@ -42,9 +65,11 @@ render model =
                 (\x -> x)
             else
                 File.purgeHidden
-    in
-        div []
+
+        completeTree =
             (model.tree
                 |> f
                 |> List.map (entry model.history.present)
             )
+    in
+        div [] ((parentFolder model) ++ completeTree)
