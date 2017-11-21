d2d1486bb65800fb69eb039559558437c76b5287
xvw <xavier.vdw@gmail.com>
2017-11-21 02:36:59 +0100

    Purge for "clean" version

--- src/Main.elm
+++ src/Main.elm
@@ -1,64 +1,7 @@
 module Main exposing (..)
 
-import Action
-import Combination
-import Html exposing (programWithFlags)
-import Architecture exposing (Message(..), Model, Flags)
-import View exposing (global)
-import Zipper.History as History
-import Port
-import Keyboard
-import Set
+import Html exposing (text)
 
 
-init : Flags -> ( Model, Cmd Message )
-init flags =
-    ( { history = History.new flags.pwd
-      , home = flags.home
-      , tree = { path = "", entries = [] }
-      , displayedTree = { path = "", entries = [] }
-      , showHidden = False
-      , searchState = ""
-      , keys = Set.empty
-      }
-    , Port.ls flags.pwd
-    )
-
-
-subscriptions : Model -> Sub Message
-subscriptions model =
-    Sub.batch
-        [ Port.getDirTree RetreiveTree
-        , Port.treeMutation TreeMutation
-        , Keyboard.downs KeyDown
-        , Keyboard.ups KeyUp
-        ]
-
-
-update : Message -> Model -> ( Model, Cmd Message )
-update message model =
-    case message of
-        Patch f ->
-            f model
-
-        RetreiveTree tree ->
-            Action.getDir tree model
-
-        TreeMutation flag ->
-            Action.treeMutation flag model
-
-        KeyUp k ->
-            Combination.keyUp k model
-
-        KeyDown k ->
-            Combination.keyDown k model
-
-
-main : Platform.Program Flags Model Message
 main =
-    programWithFlags
-        { init = init
-        , update = update
-        , view = global
-        , subscriptions = subscriptions
-        }
+    text "Yo"
--- static/js/index.js
+++ static/js/index.js
@@ -1,43 +1,6 @@
-import Electron from 'electron'
-import Path from 'path'
-import Elm from '../../src/Main.elm'
-import * as Qian from './qian.js'
-import fs from 'fs'
+import electron from 'electron'
+import elm from '../../src/Main.elm'
 
-const remote = Electron.remote
-const app = remote.app
+
 const container = document.getElementById('app');
-const homeDir = app.getPath('home')
-
-const elmApp = Elm.Main.embed(container, {
-  pwd:  homeDir
-, home: homeDir
-});
-
-
-let watcher
-
-elmApp.ports.ls.subscribe((pwd) => {
-  const path = Qian.resolvePath(pwd)
-  const list = Qian.ls(path)
-  if (watcher) {
-    watcher.close()
-  }
-  watcher = fs.watch(path, { encoding: 'buffer' }, (et, fn) => {
-    elmApp.ports.treeMutation.send(true)
-  })
-  elmApp.ports.getDirTree.send(list)
-});
-
-elmApp.ports.openFile.subscribe((path) => {
-  Qian.openFile(path)
-});
-
-elmApp.ports.openTerminal.subscribe((path) => {
-  Qian.openTerminal(path)
-});
-
-
-elmApp.ports.openInExplorer.subscribe((path) => {
-  Qian.openInFinder(path)
-})
+const elmApp = elm.Main.embed(container);
